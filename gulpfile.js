ca5c5a22ebbd9f675fc2cf1f013cc4abb2e197c9
Edgar Romero <edgarr@thehangar.cr>
2016-06-10 15:12:45 -0600

    Final configuration Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ var sass = require('gulp-sass');
 var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var del = require('del');
+var runSequence = require('run-sequence');
 var browserSync = require('browser-Sync').create();
 
 gulp.task('sass',function(){
@@ -33,5 +37,35 @@ gulp.task('useref', function(){
     return gulp.src('Pagina/*.html')
     .pipe(useref())
     .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulp.dest('dist'))
 });
+
+gulp.task('images', function(){
+    return gulp.src('Pagina/images/**/*.+(png|jpg|gif|svg)')
+    .pipe(cache(imagemin({
+        interlaced: true
+    })))
+    .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('fonts', function(){
+    return gulp.src('Pagina/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('clean:dist', function(){
+    return del.sync('dist');
+});
+
+gulp.task('build', function(callback){
+    runSequence('clean:dist',['sass','useref', 'images', 'fonts'], callback)
+});
+
+gulp.task('default', function(callback){
+    runSequence(['sass','browserSync', 'watch'], callback)
+});
+
+// gulp.task('cache:clear', function(callback){
+//     return cache.clearAll(callback);
+// });
